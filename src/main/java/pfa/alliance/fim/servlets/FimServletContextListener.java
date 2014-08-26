e595e9f379598cb8db05e29ffc651eb578e25bb7
Nemeti, D. Csaba <nd7275@gmail.com>
2014-08-27 00:22:36 +0300

    #11 Integrate Google Guice, fix logging issues, add Jetty plug-in

---
+++ src/main/java/pfa/alliance/fim/servlets/FimServletContextListener.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package pfa.alliance.fim.servlets;
+
+import javax.servlet.ServletContextEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+/**
+ * This class is used for defining the {@link GuiceServletContextListener}
+ * 
+ * 
+ * @author Csaba
+ *
+ */
+public class FimServletContextListener extends GuiceServletContextListener {
+	/** The logger used in this class. */
+	private static final Logger LOG = LoggerFactory
+			.getLogger(FimServletContextListener.class);
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		super.contextInitialized(servletContextEvent);
+		LOG.info("Fim application started...");
+	}
+
+	@Override
+	protected Injector getInjector() {
+		return Guice.createInjector(new FimServletModule());
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		LOG.info("Stopping Fim application...");
+		super.contextDestroyed(servletContextEvent);
+	}
+}
---
+++ src/main/java/pfa/alliance/fim/servlets/FimServletModule.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package pfa.alliance.fim.servlets;
+
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+
+import com.google.inject.servlet.ServletModule;
+
+/**
+ * This class is used for define the mappings for Fim servlets and filters.
+ * 
+ * @author Csaba
+ *
+ */
+class FimServletModule extends ServletModule{
+	@Override
+	protected void configureServlets() {
+		super.configureServlets();
+		
+		// binding MBean Server if necessary in future
+		bind(MBeanServer.class).toInstance(MBeanServerFactory.createMBeanServer());
+	}
+}
