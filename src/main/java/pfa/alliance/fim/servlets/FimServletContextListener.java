58a7746fc0dee862e4f4f4ff32eef77b2b5be123
Nemeti, D. Csaba <nd7275@gmail.com>
2014-09-07 13:14:20 +0300

    #41 Added dependencies and persistence XML. File is not loadedin memory

--- src/main/java/pfa/alliance/fim/servlets/FimServletContextListener.java
+++ src/main/java/pfa/alliance/fim/servlets/FimServletContextListener.java
@@ -10,34 +10,37 @@ import org.slf4j.LoggerFactory;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
 
 /**
  * This class is used for defining the {@link GuiceServletContextListener}
  * 
- * 
  * @author Csaba
- *
  */
-public class FimServletContextListener extends GuiceServletContextListener {
-	/** The logger used in this class. */
-	private static final Logger LOG = LoggerFactory
-			.getLogger(FimServletContextListener.class);
+public class FimServletContextListener
+    extends GuiceServletContextListener
+{
+    /** The logger used in this class. */
+    private static final Logger LOG = LoggerFactory.getLogger( FimServletContextListener.class );
 
-	@Override
-	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		super.contextInitialized(servletContextEvent);
-		LOG.info("Fim application started...");
-	}
+    @Override
+    public void contextInitialized( ServletContextEvent servletContextEvent )
+    {
+        super.contextInitialized( servletContextEvent );
+        LOG.info( "Fim application started..." );
+    }
 
-	@Override
-	protected Injector getInjector() {
-		return Guice.createInjector(new FimServletModule());
-	}
+    @Override
+    protected Injector getInjector()
+    {
+        return Guice.createInjector( new FimServletModule(), new JpaPersistModule( "fimJpaUnit" ) );
+    }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent servletContextEvent) {
-		LOG.info("Stopping Fim application...");
-		super.contextDestroyed(servletContextEvent);
-	}
+    @Override
+    public void contextDestroyed( ServletContextEvent servletContextEvent )
+    {
+        LOG.info( "Stopping Fim application..." );
+        super.contextDestroyed( servletContextEvent );
+    }
 }
