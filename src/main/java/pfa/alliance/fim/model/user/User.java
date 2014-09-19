1d86b9daa91464d5de65a4928d3245b16fd85ef1
Nemeti, D. Csaba <nd7275@gmail.com>
2014-09-19 23:33:29 +0300

    #26 Implement user related model + migration script

---
+++ src/main/java/pfa/alliance/fim/model/user/User.java
@@ -0,0 +1,184 @@
+/**
+ * 
+ */
+package pfa.alliance.fim.model.user;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import pfa.alliance.fim.model.GenericModel;
+
+/**
+ * This class defines a user.
+ * 
+ * @author Nemeti
+ */
+@Entity(name="fim_user")
+public class User
+    extends GenericModel
+{
+    private static final long serialVersionUID = -7478487015028567486L;
+
+    /** The user ID. */
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+    /** The first name of the user. */
+    @Column( name = "first_name", length = 100, nullable = true )
+    private String firstName;
+
+    /** The last name of the user. */
+    @Column( name = "last_name", length = 100, nullable = true )
+    private String lastName;
+
+    /** The user e-mail address. */
+    @Column( name = "email", length = 200, nullable = false, unique = true )
+    private String email;
+
+    /** The user login name. */
+    @Column( name = "login", length = 200, nullable = false, unique = true )
+    private String login;
+
+    /** The password of the user. */
+    @Column( name = "password", length = 250, nullable = false )
+    private String password;
+
+    /** The user status. */
+    @Enumerated( EnumType.STRING )
+    @Column( name = "status", length = 20, nullable = false )
+    private UserStatus status;
+
+    @OneToMany( mappedBy = "user", cascade = CascadeType.ALL )
+    @OrderBy( "id DESC" )
+    private Set<UserLogin> logins;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId( Integer id )
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName( String firstName )
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName( String lastName )
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail( String email )
+    {
+        this.email = email;
+    }
+
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public void setLogin( String login )
+    {
+        this.login = login;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword( String password )
+    {
+        this.password = password;
+    }
+
+    public UserStatus getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus( UserStatus status )
+    {
+        this.status = status;
+    }
+
+    public Set<UserLogin> getLogins()
+    {
+        return logins;
+    }
+
+    public void setLogins( Set<UserLogin> logins )
+    {
+        this.logins = logins;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        hcb.append( email );
+        hcb.append( login );
+        hcb.append( status );
+        return hcb.hashCode();
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( !( obj instanceof User ) )
+        {
+            return false;
+        }
+        User user = (User) obj;
+        EqualsBuilder eb = new EqualsBuilder();
+        eb.append( email, user.email );
+        eb.append( login, user.login );
+        eb.append( status, user.status );
+        return eb.isEquals();
+    }
+
+    @Override
+    public String toString()
+    {
+        ToStringBuilder tsb = new ToStringBuilder( this );
+        tsb.append( "username", login ).append( "email", email ).append( "status", status );
+        tsb.append( "first name", firstName ).append( "last name", lastName );
+        return tsb.toString();
+    }
+}
---
+++ src/main/java/pfa/alliance/fim/model/user/UserLogin.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package pfa.alliance.fim.model.user;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * This class keeps when the user logged in.
+ * 
+ * @author Nemeti
+ */
+@Entity( name = "user_login" )
+public class UserLogin
+    implements Serializable
+{
+    private static final long serialVersionUID = -3911814345921477177L;
+
+    /** The ID of a user login record. */
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    /** The timestamp when this occurred. */
+    @Column( name = "created_at", nullable = false )
+    @Temporal( TemporalType.TIMESTAMP )
+    private Timestamp createdAt;
+
+    /** The user to whom this record belongs. */
+    @ManyToOne( fetch = FetchType.EAGER, optional = false )
+    @JoinColumn( name = "user_id" )
+    private User user;
+
+    /** For JPA version. */
+    @Column( name = "version" )
+    @Version
+    private int version;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId( Long id )
+    {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void setCreatedAt( Timestamp createdAt )
+    {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser( User user )
+    {
+        this.user = user;
+    }
+
+    public int getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion( int version )
+    {
+        this.version = version;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        hcb.append( user.getId() );
+        hcb.append( createdAt );
+        return hcb.toHashCode();
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( !( obj instanceof UserLogin ) )
+        {
+            return false;
+        }
+        UserLogin userLogin = (UserLogin) obj;
+        EqualsBuilder eb = new EqualsBuilder();
+        eb.append( user.getId(), userLogin.user.getId() );
+        eb.append( createdAt, userLogin.createdAt );
+        return eb.isEquals();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "UserLogin[user id = " + user.getId() + ", login at = " + createdAt + "]";
+    }
+}
