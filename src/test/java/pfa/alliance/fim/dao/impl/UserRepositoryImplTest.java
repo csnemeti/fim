c79d88b103c9d02b3280729e04de0ff6dcb5e401
Nemeti, D. Csaba <nd7275@gmail.com>
2014-12-29 12:36:10 +0200

    #88 More unit tests

--- src/test/java/pfa/alliance/fim/dao/impl/UserRepositoryImplTest.java
+++ src/test/java/pfa/alliance/fim/dao/impl/UserRepositoryImplTest.java
@@ -3,11 +3,14 @@
  */
 package pfa.alliance.fim.dao.impl;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityTransaction;
 import javax.persistence.NonUniqueResultException;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -15,7 +18,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pfa.alliance.fim.model.user.OneTimeLinkType;
 import pfa.alliance.fim.model.user.User;
+import pfa.alliance.fim.model.user.UserOneTimeLink;
 import pfa.alliance.fim.model.user.UserStatus;
 
 /**
@@ -119,24 +124,73 @@ public class UserRepositoryImplTest
     {
         List<User> users = new ArrayList<User>();
         User dummy = new User();
-        users.add(dummy);
-        
+        users.add( dummy );
+
         User user = UserRepositoryImpl.uniqueResult( users );
-        
+
         Assert.assertNotNull( "Should NOT be null", user );
         Assert.assertSame( "Wrong instance", dummy, user );
     }
 
-    @Test(expected=NonUniqueResultException.class)
+    @Test( expected = NonUniqueResultException.class )
     public void test_uniqueResult_twoResults()
     {
         List<User> users = new ArrayList<User>();
         User dummy = new User();
-        users.add(dummy);
+        users.add( dummy );
         dummy = new User();
-        users.add(dummy);
-        
+        users.add( dummy );
+
         UserRepositoryImpl.uniqueResult( users );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_invalidUuidNoDesignation()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "123", null );
+        Assert.assertNull( "This link should be NULL", link );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_invalidUuidWithDesignation()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "123", OneTimeLinkType.FORGOT_PASWORD );
+        Assert.assertNull( "This link should be NULL", link );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_validUuidNoDesignation()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "uid3", null );
+        Assert.assertNotNull( "This link should NOT be NULL", link );
+        Assert.assertEquals( "Wrong UUID", "uid3", link.getUuid() );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_validUuidWrongDesignation()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "uid3", OneTimeLinkType.USER_REGISTRATION );
+        Assert.assertNull( "This link should be NULL", link );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_validUuidCorrectDesignation()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "uid3", OneTimeLinkType.FORGOT_PASWORD );
+        Assert.assertNotNull( "This link should NOT be NULL", link );
+        Assert.assertEquals( "Wrong UUID", "uid3", link.getUuid() );
+    }
+
+    @Test
+    public void test_getOneTimeLinkBy_validUuidNoDesignationAndExpired()
+    {
+        UserOneTimeLink link = userRepositoryImpl.getOneTimeLinkBy( "uid1", null );
+        // expiration check is made on business level in order to return correct error message
+        // so we should have the link, regardless of the fact that is expired
+        Assert.assertNotNull( "This link should NOT be NULL", link );
+        Assert.assertEquals( "Wrong UUID", "uid1", link.getUuid() );
+        Assert.assertTrue( "This should be expired",
+                           link.getExpiresAt().before( new Timestamp( System.currentTimeMillis() ) ) );
     }
 
     @AfterClass
